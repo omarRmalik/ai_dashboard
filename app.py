d1642bef774b2cc239220adfbabcf6f354ed70bc
omarRmalik <maliko@mail.gvsu.edu>
2024-04-20 15:36:50 -0400

    3rd commit 4-20-24

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from dash import dcc, html
 from dash.dependencies import Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
-from dash_extensions.enrich import DashProxy, html, Output, Input, dcc
 import pandas as pd
 
 # National data
